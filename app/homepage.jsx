7c8d06274506f8c478d5e7cffaf3244a6cf71099
evelyn <devynparker91@gmail.com>
2025-12-30 06:39:58 +0000

    NOW DA REAL FUN BEGINS

---
+++ app/homepage.jsx
@@ -0,0 +1,13 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const homepage = () => {
+  return (
+    <View>
+      <Text>index, yo</Text>
+    </View>
+  )
+}
+
+export default homepage
+const styles = StyleSheet.create({})
