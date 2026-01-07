05fea027f3d0558aa0435a8252ff906b9f28e164
evelyn <devynparker91@gmail.com>
2026-01-07 11:18:07 +0000

    Improved tabBar and added placeholders for home page

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,14 +1,55 @@
 import React from 'react'
 import {Tabs} from 'expo-router'
+import { Ionicons } from '@expo/vector-icons'
 
-const TabLayout = () => {
-  return (
+const TabLayout = () => { // Temporary tab bar atm
+  return (                // may use React Navigation once functionality is complete
     <Tabs>
-      <Tabs.Screen name="list" options={({title: "To-Do"})}/>
-      <Tabs.Screen name="timer" options={({title: "Timer"})}/>
-      <Tabs.Screen name="index" options={({title: "Home"})}/>
-      <Tabs.Screen name="shop" options={({title: "Shop"})}/>
-      <Tabs.Screen name="options" options={({title: "Options"})}/>
+
+      <Tabs.Screen 
+      name="list" 
+      options={({title: "To-Do", tabBarIcon: () => (
+        <Ionicons
+        size={24}
+        name='list-outline'
+        />
+      )})}/>
+      
+      <Tabs.Screen 
+      name="timer" 
+      options={({title: "Timer", tabBarIcon: () => (
+        <Ionicons
+        size={24}
+        name='timer-outline'
+        />
+      )})}/>
+      
+      <Tabs.Screen 
+      name="index" 
+      options={({title: "Home", tabBarIcon: () => (
+        <Ionicons
+        size={24}
+        name='home-outline'
+        />
+      )})}/>
+      
+      <Tabs.Screen 
+      name="shop" 
+      options={({title: "Shop", tabBarIcon: () => (
+        <Ionicons
+        size={24}
+        name='bag-outline'
+        />
+      )})}/>
+      
+      <Tabs.Screen 
+      name="options" 
+      options={({title: "Options", tabBarIcon: () => (
+        <Ionicons
+        size={24}
+        name='settings-outline'
+        />
+      )})}/>
     </Tabs>
   )
 }
---
+++ app/(tabs)/index.jsx
@@ -0,0 +1,39 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import { Image } from 'expo-image'
+
+
+const index = () => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.topmessage}>Welcome (name)! What do you want to do?</Text>
+      <Image
+      style={styles.image}
+      source="https://picsum.photos/200/300"
+      contentFit='cover'
+      />
+    </View>
+  )
+}
+
+export default index
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: "center",
+        flexDirection: "column"
+    },
+    topmessage: {
+      backgroundColor: "#5edd8bff",
+      color: "#ffff",
+      fontSize: 20,
+      padding: 20
+    },
+    image: {
+      width: "100%",
+      height: "100%",
+      flex: 1,
+      resizeMode: "cover",
+    }
+})
